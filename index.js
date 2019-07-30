3483a114f65aadd5290e2f4425b90d751f84bab4
Tomislav Vrbicic <tomislavvrbicic1997@gmail.com>
2019-07-30 11:30:24 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+var randomNumber1 = Math.floor(Math.random() * 10) + 1;
+var randomCircleColor1 = "circle" + randomNumber1 + ".png";
+var randomImagesSource1 = "images/" + randomCircleColor1;
+var circle1 = document.querySelectorAll("img")[0].setAttribute("src", randomImagesSource1);
+
+var randomNumber2 = Math.floor(Math.random() * 10) + 1;
+var randomCircleColor2 = "circle" + randomNumber2 + ".png";
+var randomImagesSource2 = "images/" + randomCircleColor2;
+var circle1 = document.querySelectorAll("img")[1].setAttribute("src", randomImagesSource2);
+
+var randomNumber3 = Math.floor(Math.random() * 10) + 1;
+var randomCircleColor3 = "circle" + randomNumber3 + ".png";
+var randomImagesSource3 = "images/" + randomCircleColor3;
+var circle1 = document.querySelectorAll("img")[2].setAttribute("src", randomImagesSource3);
+
+var randomNumber4 = Math.floor(Math.random() * 10) + 1;
+var randomCircleColor4 = "circle" + randomNumber4 + ".png";
+var randomImagesSource4 = "images/" + randomCircleColor4;
+var circle1 = document.querySelectorAll("img")[3].setAttribute("src", randomImagesSource4);
+
+var randomNumber5 = Math.floor(Math.random() * 10) + 1;
+var randomCircleColor5 = "circle" + randomNumber5 + ".png";
+var randomImagesSource5 = "images/" + randomCircleColor5;
+var circle1 = document.querySelectorAll("img")[4].setAttribute("src", randomImagesSource5);
+
+var randomNumber6 = Math.floor(Math.random() * 10) + 1;
+var randomCircleColor6 = "circle" + randomNumber6 + ".png";
+var randomImagesSource6 = "images/" + randomCircleColor6;
+var circle1 = document.querySelectorAll("img")[5].setAttribute("src", randomImagesSource6);
+
+var randomNumber7 = Math.floor(Math.random() * 10) + 1;
+var randomCircleColor7 = "circle" + randomNumber7 + ".png";
+var randomImagesSource7 = "images/" + randomCircleColor7;
+var circle1 = document.querySelectorAll("img")[6].setAttribute("src", randomImagesSource7);
+
+var randomNumber8 = Math.floor(Math.random() * 10) + 1;
+var randomCircleColor8 = "circle" + randomNumber8 + ".png";
+var randomImagesSource8 = "images/" + randomCircleColor8;
+var circle1 = document.querySelectorAll("img")[7].setAttribute("src", randomImagesSource8);
+
+var randomNumber9 = Math.floor(Math.random() * 10) + 1;
+var randomCircleColor9 = "circle" + randomNumber9 + ".png";
+var randomImagesSource9 = "images/" + randomCircleColor9;
+var circle1 = document.querySelectorAll("img")[8].setAttribute("src", randomImagesSource9);
+
+var randomNumber10 = Math.floor(Math.random() * 10) + 1;
+var randomCircleColor10 = "circle" + randomNumber10 + ".png";
+var randomImagesSource10 = "images/" + randomCircleColor10;
+var circle1 = document.querySelectorAll("img")[9].setAttribute("src", randomImagesSource10);
